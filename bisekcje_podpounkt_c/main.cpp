c1ff4213d825dfe34e8d80c4cd08b3ba73b6f530
Pomarancz <kuba.wtorkiewicz@gmail.com>
2022-02-08 22:27:05 +0100

    GÓWNO DZIAŁA

--- bisekcje_podpounkt_c/main.cpp
+++ bisekcje_podpounkt_c/main.cpp
@@ -1,14 +1,46 @@
-//
-//  main.cpp
-//  bisekcje_podpounkt_c
-//
-//  Created by Kuba Wtorkiewicz on 08/02/2022.
-//
-
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+float f(float x)
+{
+    return sin(2*x);
+}
+
+float bisekcja(float a, float b, float d)
+{
+    if(f(a) == 0.0)
+        return a;
+    if(f(b) == 0.0)
+        return b;
+    
+    float s;
+    
+    while(b - a > d)
+    {
+        s = (a + b) / 2;
+        if(f(s) == 0.0f)
+            return s;
+        if(f(a) * f(s) < 0) b = s;
+        else a = s;
+    }
+    return (a+b) / 2;
+}
+
+int main() {
+    float a=0;
+    float b=0;
+    float d=0;
+    
+    cout << "Podaj a" << endl;
+    cin >> a;
+    cout << "Podaj b" << endl;
+    cin >> b;
+    cout << "Podaj dokladnosc" << endl;
+    cin >> d;
+    
+    cout << bisekcja(a, b, d) << endl;
+    
+    
     return 0;
 }
